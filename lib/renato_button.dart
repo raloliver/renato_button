72a860b8f83c9824bb2ddb6b831625a8c05b70d2
Renato A dos Santos <40146884+Git-RenatoAlcantara@users.noreply.github.com>
2019-06-05 15:47:31 -0300

    version 1

---
+++ lib/renato_button.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+
+class RenatoButton extends StatelessWidget {
+  double width;
+  double height;
+  Color color;
+  Color progressColor;
+  Widget textButton;
+
+  RenatoButton({this.width, this.height, this.color,this.progressColor,this.textButton});
+  int statusClick = 0;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ButtonAnimationController(
+
+        width: this.width,
+        height: this.height,
+        color: this.color,
+        textButton: this.textButton,
+	      progressColor:  this.progressColor
+      );
+
+  }
+}
+
+
+class ButtonAnimationController extends StatefulWidget {
+  double width;
+  double height;
+  Color color;
+  Color progressColor;
+  Widget textButton;
+
+  ButtonAnimationController(
+      {this.width,
+      this.height,
+      this.color,
+      this.progressColor,
+      this.textButton});
+
+  @override
+  _ButtonAnimationControllerState createState() =>
+      _ButtonAnimationControllerState();
+}
+
+class _ButtonAnimationControllerState extends State<ButtonAnimationController>
+    with TickerProviderStateMixin {
+  int statusClick = 0;
+  AnimationController controller;
+  Animation<double> buttonSqueezeAnimation;
+  Animation<double> buttonZoomout;
+
+  Tween _tween;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    _tween = Tween<double>(
+        begin: widget.width == null ? 200 : widget.width, end: 70);
+
+    buttonSqueezeAnimation = _tween.animate(new CurvedAnimation(
+        parent: controller, curve: new Interval(0.0, 0.350)))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.dismissed) {
+          print("Terminou!");
+        }
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return InkWell(
+      onTap: () {
+        controller.forward();
+      },
+      child: stateInitial(),
+    );
+  }
+
+  Container stateInitial() {
+    return Container(
+        width: buttonSqueezeAnimation.value,
+        height: widget.height == null ? 60.0 : widget.height,
+        decoration: BoxDecoration(
+          color: this.widget.color,
+          borderRadius: new BorderRadius.all(const Radius.circular(50.0)),
+        ),
+        child: buttonSqueezeAnimation.value > 75.0
+            ? new Center(
+                child: widget.textButton == null
+                    ? Text("Animation")
+                    : widget.textButton,
+              )
+            : new CircularProgressIndicator(
+                valueColor: new AlwaysStoppedAnimation<Color>(
+                  widget.progressColor == null
+                      ? Colors.purple
+                      : widget.progressColor,
+                ),
+              ));
+  }
+}
+
+
